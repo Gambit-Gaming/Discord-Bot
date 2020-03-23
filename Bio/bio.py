d33a1384fa65ef83a1e6666fc6b86b18f249083d
Caleb <cmwhite@pm.me>
2020-03-23 15:26:08 -0400

    Rewrite

--- Bio/bio.py
+++ Bio/bio.py
@@ -6,32 +6,85 @@ from collections import namedtuple
 from typing import Optional
 
 import discord
-from redbot.core import Config, commands, bot
+from redbot.core import checks, Config, commands, bot
 
 log = logging.getLogger("red.cogs.bio")
 
 __all__ = ["UNIQUE_ID", "Bio"]
 
 UNIQUE_ID = 0x62696F68617A617264
-MemberBio = namedtuple("MemberBio", "bio")
 
 
 class Bio(commands.Cog):
     def __init__(self, bot: bot.Red):
         self.conf = Config.get_conf(self, identifier=UNIQUE_ID, force_registration=True)
-        self.conf.register_user(bio="{}")
+        self.conf.register_user(bio='{}')
+        self.conf.register_guild(biofields='{"fields": []}')
+
+    @commands.command()
+    @checks.admin()
+    async def biofields(self, ctx: commands.Context, command: str = None, *args):
+        bioFields = json.loads(await self.conf.guild(ctx.guild).biofields())
+        if not command:
+            await ctx.send("\n".join(bioFields["fields"]))
+            return
+        argField = " ".join(args)
+        if command == "add":
+            bioFields["fields"].append(argField)
+        elif command == "remove":
+            # TODO: Delete field from each user's bio
+            try:
+                bioFields["fields"].remove(argField)
+            except:
+                await ctx.send(f"No field named '{argField}'")
+                return
+        else:
+            await ctx.send(f"Unknown command: {command}")
+        await self.conf.guild(ctx.guild).biofields.set(json.dumps(bioFields))
+        await ctx.send("Field '{argField}' has been {command[0:5]}ed")
+        if command == "remove":
+            count = 0
+            for member, conf in (await self.conf.all_users()).items():
+                memberBio = json.loads(conf.get("bio"))
+                if argField in memberBio.keys():
+                    del memberBio[argField]
+                    await self.conf.user(member).bio.set(json.dumps(memberBio))
+                    count += 1
+            await ctx.send(f"Removed field '{argField}' from {count} bios")
 
     @commands.command()
     async def bio(self, ctx: commands.Context, user = None, *args):
+        bioFields = json.loads(await self.conf.guild(ctx.guild).biofields())
         key = None
         if not isinstance(user, discord.Member):
-            if user and args:
-                # Argument is a key to set, not a user
+            # Argument is a key to set, not a user
+            if user:
                 key = user
-            user = ctx.author
-        bioDict = json.loads(await self.conf.user(user).bio())
-        if key and args:
+                user = None
+        _user = user or ctx.author
+        bioDict = json.loads(await self.conf.user(_user).bio())
+
+        # User is setting own bio
+        if key is not None and _user == ctx.author:
+            if key not in bioFields["fields"]:
+                await ctx.send("Sorry, that bio field is not available.\n"
+                               "Please request it from the server owner.")
+                return
             bioDict[key] = " ".join(args)
             await self.conf.user(user).bio.set(json.dumps(bioDict))
-        else:
-            await ctx.send(f"{bioDict}")
+            await ctx.send(f"{key} set to {bioDict[key]}")
+            return
+
+        # Filter dict to key(s)
+        elif user and len(args):
+            data = {}
+            for arg in args:
+                try:
+                    data[arg] = bioDict[arg]
+                except:
+                    await ctx.send(f"Field '{arg}' not found")
+            # TODO: Pretty up this output
+            await ctx.send(bioDict[key])
+            bioDict = data
+        # TODO: Pretty up this output
+        await ctx.send(f"{bioDict}")
