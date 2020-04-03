dfbabf0699c9bf3b96d8c9662a0b6998dc5e82d3
Caleb <cmwhite@pm.me>
2020-04-03 19:06:11 -0400

    First attempt at implementing new Discord tasks feature

--- Tube/__init__.py
+++ Tube/__init__.py
@@ -2,6 +2,4 @@
 from .tube import Tube
 
 async def setup(bot):
-    cog = Tube(bot)
-    bot.add_cog(cog)
-    cog.init()
+    bot.add_cog(Tube(bot))
--- Tube/tube.py
+++ Tube/tube.py
@@ -11,6 +11,7 @@ import feedparser
 
 from typing import Optional
 
+from discord.ext import tasks
 from redbot.core import Config, bot, checks, commands
 
 log = logging.getLogger("red.cbd-cogs.tube")
@@ -30,22 +31,7 @@ class Tube(commands.Cog):
         self.conf.register_guild(subscriptions=[])
         self.conf.register_global(interval=300)
         self.session = aiohttp.ClientSession()
-        self.bg_loop_task: Optional[asyncio.Task] = None
-
-    def init(self):
-        self.bg_loop_task = asyncio.create_task(self.bg_loop())
-
-        def done_callback(future: asyncio.Future):
-            try:
-                future.exception()
-            except asyncio.CancelledError:
-                pass
-            except asyncio.InvalidStateError as e:
-                log.exception("Callback state invalid: ", exc_info=e)
-            except Exception as e:
-                log.exception("Unhandled exception: ", exc_info=e)
-
-        self.bg_loop_task.add_done_callback(done_callback)
+        self.background_get_new_videos.start()
 
     @commands.group()
     async def tube(self, ctx: commands.Context):
@@ -217,6 +203,7 @@ class Tube(commands.Cog):
     @tube.command(name="setinterval", hidden=True)
     async def set_interval(self, ctx: commands.Context, interval: int):
         await self.conf.interval.set(interval)
+        self.background_get_new_videos.change_interval(seconds=interval)
         await ctx.send(f"Interval set to {await self.conf.interval()}")
 
     async def get_feed(self, channel):
@@ -231,16 +218,18 @@ class Tube(commands.Cog):
         return res
 
     def cog_unload(self):
-        if self.bg_loop_task:
-            self.bg_loop_task.cancel()
-        asyncio.create_task(self.session.close())
+        self.background_get_new_videos.cancel()
 
-    async def bg_loop(self):
+    @tasks.loop(seconds=1)
+    async def background_get_new_videos(self):
+        fetched = {}
+        for guild in self.bot.guilds:
+            update = await self._get_new_videos(guild, fetched)
+            if not update:
+                continue
+            fetched.update(update)
+
+    @background_get_new_videos.before_loop
+    async def wait_for_red(self):
         await self.bot.wait_until_red_ready()
-        while await asyncio.sleep(await self.conf.interval(), True):
-            fetched = {}
-            for guild in self.bot.guilds:
-                update = await self._get_new_videos(guild, fetched)
-                if not update:
-                    continue
-                fetched.update(update)
+        self.background_get_new_videos.change_interval(seconds=await self.conf.interval())
