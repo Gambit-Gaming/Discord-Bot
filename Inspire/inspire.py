be00c1a7321b9ce989fce434c634d123d4505b8d
Caleb <cmwhite@pm.me>
2021-02-07 18:32:28 -0500

    Check embed permissions in a better way (#15)

--- Inspire/inspire.py
+++ Inspire/inspire.py
@@ -8,11 +8,9 @@ class Inspire(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    @commands.bot_has_permissions(embed_links=True)
     async def inspire(self, ctx: commands.Context):
         """ Become someone who is inspired """
-        if not ctx.channel.permissions_for(ctx.guild.me).embed_links:
-            await ctx.send("They won't let me do that here.")
-            return
         inspiration = inspirobot.generate()
         embed = discord.Embed(url=inspiration.url, title = f"Inspiration for {ctx.author.display_name}", color=await ctx.embed_color())
         embed.set_image(url=inspiration.url)
